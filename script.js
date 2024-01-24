6af51afd020d989e45f9a900475fcbc7d5b26d33
TheMuno <143614176+TheMuno@users.noreply.github.com>
2024-01-24 07:26:09 +0000

    Day-time select dropdown functionality

--- script.js
+++ script.js
@@ -48,7 +48,8 @@ const $map = document.querySelector('#map'),
     $hourlyBtn = document.querySelector('.view-hourly'),
     $addReservation = document.querySelector('.add-reservation'),
     $reservations = document.querySelector('.reservations'),
-    $hourEvents = document.querySelector('.hour-events');
+    $hourEvents = document.querySelector('.hour-events'),
+    $dayTimesSelect = document.querySelector('.day-times');
     
     $userMail.value = localStorage.getItem('user-email') || 'one@mail.com'; 
     
@@ -1169,74 +1170,12 @@ $hourlyBtn.addEventListener('click', e => {
 }); 
 
 
-/*
-const SPREADSHEET_ID = '1Zj1ae5faA8h7UwHvtYVtKoiA_G3LtQcuTOFV1Evq4BQ'; 
-const CLIENT_ID = '424207365074-g5444q1mhm5n5d04pu563ofqjup3j5e2.apps.googleusercontent.com'; 
-const API_KEY = 'AIzaSyCMmi6kGAOGfMzK4CBvNiVBB7T6OjGbsU4'; 
-const SCOPE = 'https://www.googleapis.com/auth/spreadsheets,https://www.googleapis.com/auth/drive,https://www.googleapis.com/auth/drive.file'; 
+$dayTimesSelect.addEventListener('change', e => {
+    const $selectedTime = $dayTimesSelect.value;
+    const $hrEvents = $dayTimesSelect.closest('.hour-events '); 
+    const $otherTimes = $hrEvents.querySelectorAll(`.day-time:not(.${$selectedTime})`); 
 
-!function handleClientLoad() { //initialize the Google API
-    gapi.load('client:auth2', initClient);
-}();
+    $hrEvents.querySelector(`.${$selectedTime}`)?.classList.remove('hide');
+    $otherTimes.forEach(time => time.classList.add('hide'));
+});
 
-function initClient() { //provide the authentication credentials you set up in the Google developer console
-    console.log('gapi',gapi)  
-   
-    gapi.client.init({
-      'apiKey': API_KEY,
-      'clientId': CLIENT_ID,
-      'scope': SCOPE,
-      'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-    }).then(()=> {
-      gapi.auth2.getAuthInstance().isSignedIn.listen(this.updateSignInStatus); //add a function called `updateSignInStatus` if you want to do something once a user is logged in with Google
-      this.updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    });
-}
-
-function onFormSubmit(submissionValues) {
-
-    const params = {
-      // The ID of the spreadsheet to update.
-      spreadsheetId: SPREADSHEET_ID, 
-      // The A1 notation of a range to search for a logical table of data.Values will be appended after the last row of the table.
-      range: 'Sheet1', //this is the default spreadsheet name, so unless you've changed it, or are submitting to multiple sheets, you can leave this
-      // How the input data should be interpreted.
-      valueInputOption: 'RAW', //RAW = if no conversion or formatting of submitted data is needed. Otherwise USER_ENTERED
-      // How the input data should be inserted.
-      insertDataOption: 'INSERT_ROWS', //Choose OVERWRITE OR INSERT_ROWS
-    };
-
-    const valueRangeBody = {
-      'majorDimension': 'ROWS', //log each entry as a new row (vs column)
-      'values': [submissionValues] //convert the object's values to an array
-    };
-
-    let request = gapi.client.sheets.spreadsheets.values.append(params, valueRangeBody);
-    request.then(function (response) {
-      // TODO: Insert desired response behaviour on submission
-      console.log(response.result);
-    }, function (reason) {
-      console.error('error: ' + reason.result.error.message);
-    });
-}
-
-setTimeout(() => {
-    console.log('ran!!')
-    onFormSubmit('My, Name, Is, This'); 
-}, 20 * 1000); 
-// onFormSubmit('My, Name, Is, This') 
-
-
-*/
-
-
-  /**
-   * Sample JavaScript code for sheets.spreadsheets.values.append
-   * See instructions for running APIs Explorer code samples locally:
-   * https://developers.google.com/explorer-help/code-samples#javascript
-   */
-
-  
-
-{/* <button onclick="authenticate().then(loadClient)">authorize and load</button>
-<button onclick="execute()">execute</button> */} 
