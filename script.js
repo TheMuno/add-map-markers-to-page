502ef5ac13863fa0816846f08b13d9609f4b422a
TheMuno <143614176+TheMuno@users.noreply.github.com>
2024-02-28 10:28:55 +0000

    Stop deletedDays retrieval

--- script.js
+++ script.js
@@ -936,7 +936,7 @@ async function retrieveSavedMarkersFromFirebase(userMail) {
     // console.log('days in db:', days)
 
     setupDays('.all-days', days); 
-    setupDays('.removed-days .all-days', deletedDays); 
+    // setupDays('.removed-days .all-days', deletedDays); 
 
     function setupDays($parentContainer, daysArr) {
         const $allDays = $dayActivities.querySelector($parentContainer); 
