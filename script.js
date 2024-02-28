fe1f223c78872c67db6ae00476a37c4e1cb2e1fc
TheMuno <143614176+TheMuno@users.noreply.github.com>
2024-02-28 19:25:30 +0000

    Add keydown event on window to test

--- script.js
+++ script.js
@@ -1622,6 +1622,43 @@ $mapResultsContent.addEventListener('keydown', e => {
     // handleMapOverlayClose();
 });
 
+window.addEventListener('keydown', e => {
+      if (e.defaultPrevented) {
+        return; // Do nothing if the event was already processed
+      }
+  
+      switch (e.key) {
+        case "ArrowDown":
+          // Do something for "down arrow" key press.
+          break;
+        case "ArrowUp":
+          // Do something for "up arrow" key press.
+          break;
+        case "ArrowLeft":
+          // Do something for "left arrow" key press.
+          break;
+        case "ArrowRight":
+          // Do something for "right arrow" key press.
+          break;
+        case "Enter":
+          // Do something for "enter" or "return" key press.
+          break;
+        case " ":
+          // Do something for "space" key press.
+          break;
+        case "Escape":
+          // Do something for "esc" key press.
+          break;
+        default:
+          return; // Quit when this doesn't handle the key event.
+      }
+  
+      // Cancel the default action to avoid it being handled twice
+      e.preventDefault();
+    },
+    true,
+);
+
 async function createNSaveMarkerToDB({mapPlaceObject:place, dayEventName, dayIdentifier, dayDate}) {
     const { marker } = createMarker(place);   
 
