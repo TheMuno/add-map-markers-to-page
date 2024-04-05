6336d9146c82252651bc23991f3af7653f635a52
TheMuno <143614176+TheMuno@users.noreply.github.com>
2024-04-05 13:28:02 +0100

    Update filter script

--- script.js
+++ script.js
@@ -1117,7 +1117,7 @@ async function saveMarkerToFirebase(userMail, dayDate, markerObj) {
     const dayObj = {}; 
     dayObj.days = days; 
     // if ($addToHiveBtn.checked) dayObj.hive = arrayUnion(dayEventName); 
-    // dayObj.hive = arrayUnion(hiveObj); 
+    dayObj.hive = arrayUnion(hiveObj); 
     dayObj.modifiedAt = serverTimestamp(); 
 
     // console.log('Saved to:', dayNum, 'days', days)  
