d354cadcd925255c39c97ffd8745a48c4e549cb1
TheMuno <143614176+TheMuno@users.noreply.github.com>
2024-05-08 07:58:11 +0100

    Log activeCheckboxes.length

--- hive-script.js
+++ hive-script.js
@@ -425,6 +425,8 @@ $hiveFilterCheckboxes.forEach(checkbox => {
 
         if (!activeCheckboxes.length && $hiveList.querySelectorAll('.hive-item:not(.hide)').length === 0) {
             console.log('HIT::::::::::::::::::::::::::::::::::::::::::::')
+            console.log('activeCheckboxes', activeCheckboxes)
+            console.log('activeCheckboxes.length', activeCheckboxes.length)
             $hiveItems.forEach(item => item.classList.remove('hide'));
             $hiveList.markers.forEach(marker => marker.setMap(map)); 
         }
